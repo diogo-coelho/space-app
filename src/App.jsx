29b2e458d5a43fc90058919226f7fbc3b617488a
Diogo Coelho <diogooliveiracoelho@gmail.com>
2024-05-20 18:13:27 -0300

    Galeria de imagens

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import { styled } from "styled-components"
+import { useState } from "react"
+import bannerBackground from './assets/banner.png'
+
 import EstilosGlobais from "./componentes/EstilosGlobais"
 import Cabecalho from "./componentes/Cabecalho"
 import BarraLateral from "./componentes/BarraLateral"
+import Banner from "./componentes/Banner"
+import Galeria from './componentes/Galeria'
+
+import fotos from './fotos.json'
 
 const FundoGradiente = styled.div`
 	background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -9,12 +16,62 @@ const FundoGradiente = styled.div`
 	min-height: 100vh;
 `
 
-function App() {
+const AppContainer = styled.div`
+	width: 1440px;
+	max-width: 100%;
+	margin: 0 auto;
+`
+
+const MainContainer = styled.main`
+	display: flex;
+	gap: 24px;
+`
+
+const ConteudoGaleria = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`
+
+const App = () => {
+  const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos)
+  const [fotoSelecionada, setFotoSelecionada] = useState(null)
+
+  const aoAlternarFavorito = (foto) => {
+    if (foto.id === fotoSelecionada?.id) {
+      setFotoSelecionada({
+        ...fotoSelecionada,
+        favorita: !fotoSelecionada.favorita
+      })
+    }
+    setFotosDaGaleria(fotosDaGaleria.map(fotoDaGaleria => {
+      return {
+        ...fotoDaGaleria,
+        favorita: fotoDaGaleria.id === foto.id ? !foto.favorita : fotoDaGaleria.favorita
+      }
+    }))
+  }
+
   return (
     <FundoGradiente>
 		<EstilosGlobais />
-		<Cabecalho />
-		<BarraLateral />
+		<AppContainer>
+			<Cabecalho />
+			<MainContainer>
+				<BarraLateral />
+				<ConteudoGaleria>
+					<Banner
+						texto="A galeria mais completa de fotos do espaço!"
+						backgroundImage={bannerBackground}
+					/>
+					<Galeria
+						fotos={fotosDaGaleria}
+						aoFotoSelecionada={foto => setFotoSelecionada(foto)} 
+              			aoAlternarFavorito={aoAlternarFavorito}
+					/>
+				</ConteudoGaleria>
+			</MainContainer>
+		</AppContainer>
     </FundoGradiente>
   )
 }
