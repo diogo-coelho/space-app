9e792e51c25c621605c6253dd3b75249064c7cfd
Diogo Coelho <diogooliveiracoelho@gmail.com>
2024-05-20 19:02:54 -0300

    Favoritos

--- src/componentes/Galeria/Imagem/index.jsx
+++ src/componentes/Galeria/Imagem/index.jsx
@@ -45,21 +45,23 @@ const Imagem = ({ foto, expandida = false, aoZoomSolicitado, aoAlternarFavorito 
     }
 
 
-    return (<Figure $expandida={expandida} id={`foto-${foto.id}`}>
-        <img src={foto.path} alt={foto.alt} />
-        <figcaption>
-            <h3>{foto.titulo}</h3>
-            <Rodape>
-                <h4>{foto.fonte}</h4>
-                <BotaoIcone onClick={() => aoAlternarFavorito(foto)}>
-                    <img src={iconeFavorito} alt="Icone de favorito" />
-                </BotaoIcone>
-                {!expandida && <BotaoIcone aria-hidden={expandida} onClick={() => aoZoomSolicitado(foto)}>
-                    <img src="/icones/expandir.png" alt="Icone de expandir" />
-                </BotaoIcone>}
-            </Rodape>
-        </figcaption>
-    </Figure>)
+    return (
+		<Figure $expandida={expandida} id={`foto-${foto.id}`}>
+			<img src={foto.path} alt={foto.alt} />
+			<figcaption>
+				<h3>{foto.titulo}</h3>
+				<Rodape>
+					<h4>{foto.fonte}</h4>
+					<BotaoIcone onClick={() => aoAlternarFavorito(foto)}>
+						<img src={iconeFavorito} alt="Icone de favorito" />
+					</BotaoIcone>
+					{!expandida && <BotaoIcone aria-hidden={expandida} onClick={() => aoZoomSolicitado(foto)}>
+						<img src="/icones/expandir.png" alt="Icone de expandir" />
+					</BotaoIcone>}
+				</Rodape>
+			</figcaption>
+		</Figure>
+	)
 }
 
 export default Imagem
